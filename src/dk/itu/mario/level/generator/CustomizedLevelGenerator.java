6bd8159ad4119472b7aa367372a2773e96717e35
Michael Sandt <mike@mike-Lenovo-U410.(none)>
2013-03-04 21:21:29 -0500

    Initial commit of files. Should already be set to run custom games.

---
+++ src/dk/itu/mario/level/generator/CustomizedLevelGenerator.java
@@ -0,0 +1,25 @@
+package dk.itu.mario.level.generator;
+
+import java.util.Random;
+
+import dk.itu.mario.MarioInterface.Constraints;
+import dk.itu.mario.MarioInterface.GamePlay;
+import dk.itu.mario.MarioInterface.LevelGenerator;
+import dk.itu.mario.MarioInterface.LevelInterface;
+import dk.itu.mario.level.CustomizedLevel;
+import dk.itu.mario.level.MyLevel;
+
+public class CustomizedLevelGenerator implements LevelGenerator{
+
+	public LevelInterface generateLevel(GamePlay playerMetrics) {
+		LevelInterface level = new CustomizedLevel(320,15,new Random().nextLong(),1,1,playerMetrics);
+		return level;
+	}
+
+	@Override
+	public LevelInterface generateLevel(String detailedInfo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
